39ab530c4d20a9cf6a67d1e579f0652242b219e1
jolson615 <jolson615@gmail.com>
2017-07-26 16:51:01 -0400

    Solution erased

--- profile_class.rb
+++ profile_class.rb
@@ -1,15 +1,5 @@
 class Profile
 
-  def initialize(name)
-    @name = name
-  end
 
-  def name
-    return @name
-  end
-
-  def name=(new_name)
-    @name = new_name
-  end
 
 end
--- test.rb
+++ test.rb
@@ -1,9 +1,1 @@
 require_relative 'profile_class.rb'
-
-first_profile = Profile.new("Nicki")
-second_profile = Profile.new("Sarah")
-
-puts first_profile.name
-
-first_profile.name = "Nicki the Great"
-puts(first_profile.name) #print out their name to make sure the change worked.
